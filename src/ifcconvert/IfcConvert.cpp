b23ec786454068c56b3517eb9355c73e8d16e41d
blocovin <kevin@blocinbloc.com>
2020-06-30 10:19:09 +0200

    Add BIB in version number

--- src/ifcconvert/IfcConvert.cpp
+++ src/ifcconvert/IfcConvert.cpp
@@ -85,7 +85,7 @@ namespace po = boost::program_options;
 
 void print_version()
 {
-    cout_ << "IfcOpenShell IfcConvert " << IFCOPENSHELL_VERSION << " (OCC " << OCC_VERSION_STRING_EXT << ")\n";
+    cout_ << "IfcOpenShell IfcConvert " << IFCOPENSHELL_VERSION << " BIB 08-12-2020 0387783 (OCC " << OCC_VERSION_STRING_EXT << ")\n";
 }
 
 void print_usage(bool suggest_help = true)
